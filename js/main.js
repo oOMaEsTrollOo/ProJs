d058f562d01cff7bef91da37aa243dca2236d265
oOMaEsTrollOo <denisov_den777@mail.ru>
2021-06-10 21:11:42 +0500

    lesson-2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,71 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000, img: 'notebook.jpg' },
-    { id: 2, title: 'Mouse', price: 20, img: 'mouse.jpg' },
-    { id: 3, title: 'Keyboard', price: 200, img: 'keyboard.jpg' },
-    { id: 4, title: 'Gamepad', price: 50, img: 'gamepad.jpg' },
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (x) => {
-    return `<div class="product-item">
-                <img src="./img/${x.img}" alt="${x.img}" width="200px" height="300px">
-                <h3 class="title">${x.title}</h3>
-                <p class="price">${x.price}</p>
+'use strict'
+
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.goodsList();
+    }
+
+    goodsList() {
+        let sumPrice = 0
+        for (let i = 0; i < this.goods.length; i++) {
+            sumPrice += this.goods[i].price;
+        }
+    }
+
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ]
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render())
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+            this.title = product.title;
+            this.price = product.price;
+            this.id = product.id;
+        }
+        //Функция для формирования верстки каждого товара
+    render() {
+        return `<div class="product-item">
+                <img src="./img/${this.title}.jpg" alt="${this.title}" width="200px" height="300px">
+                <h3 class="title">${this.title}</h3>
+                <p class="price">${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join(' ');
-};
+    }
+}
 
-renderPage(products);
+// class Cart {
+//     constructor(cart1 = '.cart') {
+//         this.cart1 = cart1;
+//     }
+
+//     clearCart()
+// }
+
+// class CartItem {
+//     constructor(cartitem1 = '.cart') {
+//         this.cartitem1 = cartitem1;
+//     }
+
+//     addProduct()
+
+//     delProduct()
+// }
+
+let list = new ProductList();
+list.render();
