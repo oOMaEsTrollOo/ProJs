264aad388aa5aa22d29757a9977e7a0abada1843
oOMaEsTrollOo <denisov_den777@mail.ru>
2021-06-17 19:20:38 +0500

    lesson-3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,53 @@
 'use strict'
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
 
+// class ProductCart {
+//     constructor(container = '.products') {
+//         this.container = container;
+//         this.goods = [];
+//         this._getProducts()
+//             .then(data => {
+//                 this.goods = [...data.contents];
+//                 amount = data.amount;
+//                 countGoods = data.countGoods;
+//                 this.render()
+//             });
+//     }
+//     _getProducts() {
+
+//         return fetch(`${API}/getBasket.json`)
+//             .then(result => result.json())
+//             .catch(error => {
+//                 console.error();
+//             })
+//     }
+
+//     addGoods() {
+
+//     }
+//     removeGoods() {
+
+//     }
+//     changeGoods() {
+
+//     }
+//     render() {
+//         const block = document.querySelector(this.cart);
+//         for (let product of this.goods) {
+//             const productObj = new CartItem(product);
+//             block.insertAdjacentHTML('beforeend', productObj.render())
+//         }
+//     }
+// }
+
+
+
+// let amount
+// let countGoods
+// let list = new ProductList();
+// let cart = new ProductCart();
+// list.render();
+
 class ProductList {
     constructor(container = '.products') {
         this.container = container;
@@ -10,8 +57,15 @@ class ProductList {
                 this.goods = [...data];
                 this.render()
             });
-        // this._fetchProducts();
         this.goodsList();
+        this._init();
+    }
+
+    goodsList() {
+        let sumPrice = 0
+        for (let i = 0; i < this.goods.length; i++) {
+            sumPrice += this.goods[i].price;
+        }
     }
 
     _getProducts() {
@@ -23,21 +77,17 @@ class ProductList {
             })
     }
 
-    goodsList() {
-        let sumPrice = 0
-        for (let product of this.goods) {
-            sumPrice += product.price;
+    _init() {
+        function cartopen(e) {
+            if (e.target.children[0].style.display == "none" || e.target.children[0].style.display == "")
+                e.target.children[0].style.display = "block"
+            else e.target.children[0].style.display = "none"
         }
+        let cartlist = document.querySelector(".cartlist");
+        let btncart = document.querySelector(".btn-cart");
+        btncart.addEventListener('click', cartopen)
     }
 
-    // _fetchProducts() {
-    //     this.goods = [
-    //         { id: 1, title: 'Notebook', price: 2000 },
-    //         { id: 2, title: 'Mouse', price: 20 },
-    //         { id: 3, title: 'Keyboard', price: 200 },
-    //         { id: 4, title: 'Gamepad', price: 50 },
-    //     ]
-    // }
     render() {
         const block = document.querySelector(this.container);
         for (let product of this.goods) {
@@ -56,77 +106,30 @@ class ProductItem {
         //Функция для формирования верстки каждого товара
     render() {
         return `<div class="product-item">
-                <img src="./img/${this.price}.jpg" alt="${this.price}" width="200px" height="300px">
-                <h3 class="title">${this.title}</h3>
-                <p class="price">${this.price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
+                    <img src="./img/${this.price}.jpg" alt="${this.price}" width="200px" height="300px">
+                    <h3 class="title">${this.title}</h3>
+                    <p class="price">${this.price}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>`
     }
 }
 
-
-class ProductCart {
-    constructor(container = '.products') {
-        this.container = container;
-        this.goods = [];
-        this._getProducts()
-            .then(data => {
-                this.goods = [...data.contents];
-                amount = data.amount;
-                countGoods = data.countGoods;
-                this.render()
-            });
-    }
-    _getProducts() {
-
-        return fetch(`${API}/getBasket.json`)
-            .then(result => result.json())
-            .catch(error => {
-                console.error();
-            })
+class Cart {
+    constructor(cart1 = '.cart') {
+        this.cart1 = cart1;
     }
 
-    addGoods() {
-
-    }
-    removeGoods() {
-
-    }
-    changeGoods() {
-
-    }
-    render() {
-        const block = document.querySelector(this.cart);
-        for (let product of this.goods) {
-            const productObj = new CartItem(product);
-            block.insertAdjacentHTML('beforeend', productObj.render())
-        }
-    }
+    clearCart() {}
 }
-
 class CartItem {
-    constructor(product) {
-            this.title = product.product_name;
-            this.price = product.price;
-            this.id = product.id_product;
-        }
-        //Функция для формирования верстки каждого товара
-    render() {
-        return `<div class="cart-item">
-                    <img class="img" src="./img/${this.price}.jpg" alt="${this.price}" width="100px" height="120px">
-                    <h3 class="title2">${this.title}</h3>
-                    <p class="price2">${this.price}</p>
-                    <button class="close ${this.price}" type="button"></button>
-                 </div>`
+    constructor(cartitem1 = '.cart') {
+        this.cartitem1 = cartitem1;
     }
+
+    addProduct() {}
+
+    delProduct() {}
 }
 
-// class ElemBasket() {
-//     render() {}
-// }
-
-let amount
-let countGoods
 let list = new ProductList();
-// let cart = new ProductCart();
 list.render();
